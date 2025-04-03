18bf14fcca647414757e252b82f5d388c7905e7c
ire-west <kaka@gm.com>
2025-04-03 15:31:55 +0200

    attention

--- FlashCareer/server.py
+++ FlashCareer/server.py
@@ -13,3 +13,26 @@ app = Flask("flashcareer")
 app.secret_key = b'\xc4*\xc1P\x01M\xbdo\x92kv\x8a|\xb5\x18q'
 MakoTemplates(app)
 SQLiteExtension(app)
+
+
+@app.route("/inscription_chercheur", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("inscription_chercheur.html.mako", error=None)
+    elif request.method == "POST":
+        db = get_db()
+        try:
+            db.execute(
+                """
+                INSERT INTO Users (type, genre, nom, prénom, email, domaine, mdp)
+                VALUES (?, ?, ?, ?, ?, ?, ?);
+                """,
+                (request.form["genre"], request.form["nom"],request.form["prénom"],request.form["email"],request.form["type"],request.form["email"],request.form["domaine"],request.form["mdp"]))
+            db.commit()
+            return redirect(url_for("accueil"), code=303)
+        except ValidationError as e:
+            return render_template("register.html.mako", error=str('c pa bien'))
+        except IntegrityError as i:
+            return render_template("register.html.mako", error=str('c pa bien'))
+        finally : 
+            db.rollback()
