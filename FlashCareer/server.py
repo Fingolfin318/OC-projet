f5de0fb47d59192c926aa3c35004964a1b7e5198
Guillaume Kaufmann <guillaume.kaufmann@edu.vs.ch>
2025-04-03 14:39:22 +0200

    orga3

--- FlashCareer/server.py
+++ FlashCareer/server.py
@@ -1,6 +1,6 @@
 import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
+##
 from flask import Flask, abort, session   # Importe le type Flask.
 from datetime import datetime
 from flask_mako import render_template, MakoTemplates
@@ -8,7 +8,9 @@ from flask_sqlite import SQLiteExtension, get_db
 from random import randint
 from flask import request, redirect, url_for
 from sqlite3 import IntegrityError
+##
 app = Flask("flashcareer") 
 app.secret_key = b'\xc4*\xc1P\x01M\xbdo\x92kv\x8a|\xb5\x18q'
 MakoTemplates(app)
 SQLiteExtension(app)
+##
